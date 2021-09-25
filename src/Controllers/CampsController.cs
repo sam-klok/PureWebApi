650c907e680b0f032531f3af8a0cff1fefec568c
sam-klok <sklok11@gmail.com>
2021-09-25 10:14:46 -0700

    adding simple get method

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PureWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        public object Get()
+        {
+            return new { Moniker = "ATL2018", Name = "Atlanat Code" };
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return null;
+        //    //return View();
+        //}
+    }
+}
